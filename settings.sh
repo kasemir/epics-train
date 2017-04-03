a9fc8177b475d58fbd9210ed3c57f6797c10474f
training <training@localhost.localdomain>
2017-04-03 14:55:20 -0400

    Add V4

--- settings.sh
+++ settings.sh
@@ -3,11 +3,12 @@
 # source ~/epics-train/settings.sh
 
 export EPICS_BASE=$HOME/epics-train/tools/base-3.15.5
+export EPICS_BASE4=$HOME/epics-train/tools/EPICS-CPP-4.6.0
 export EPICS_HOST_ARCH=linux-x86_64
 export EPICS_CA_ADDR_LIST=127.0.0.1
-export JAVA_HOME=/home/epics-train/tools/jdk1.8.0_121
+export JAVA_HOME=$HOME/epics-train/tools/jdk1.8.0_121
 
-export PATH=$EPICS_BASE/bin/$EPICS_HOST_ARCH:$JAVA_HOME/bin:/home/training/bin:$PATH
+export PATH=$EPICS_BASE4/pvAccessCPP/bin/linux-x86_64:$EPICS_BASE/bin/$EPICS_HOST_ARCH:$JAVA_HOME/bin:/home/training/bin:$PATH
 
 xinput set-prop "VirtualBox mouse integration" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1.0
 xinput set-prop "VirtualBox mouse integration" "Coordinate Transformation Matrix" 1.0 0 0 0 1.0 0 0 0 1.0
