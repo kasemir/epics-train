886d39c0559a976a96a1ab803806ddef52457f42
training <training@training.epics>
2017-04-28 17:19:20 -0400

    Add standalone display runtime

--- settings.sh
+++ settings.sh
@@ -14,6 +14,7 @@ export EPICS_PVA_ADDR_LIST="10.0.2.15"
 export EPICS_PVA_AUTO_ADDR_LIST=NO
 export JAVA_HOME=$HOME/epics-train/tools/jdk1.8.0_121
 export PYTHONPATH=$EPICS_BASE4/pvaPy/lib/python/2.7/linux-x86_64
+export SWT_GTK3=0
 
 export PATH=$EPICS_BASE4/pvAccessCPP/bin/linux-x86_64:$EPICS_BASE/bin/$EPICS_HOST_ARCH:$JAVA_HOME/bin:/home/training/bin:$PATH
 
@@ -21,9 +22,9 @@ xinput set-prop "VirtualBox mouse integration" "Coordinate Transformation Matrix
 xinput set-prop "VirtualBox mouse integration" "Coordinate Transformation Matrix" 1.0 0 0 0 1.0 0 0 0 1.0
 
 alias css=~/epics-train/tools/start_css.sh
+alias start_display=~/epics-train/tools/start_display.sh
 alias start_iocCA="cd ~/epics-train/examples/iocBoot/iocCA; ./st.cmd"
 alias start_iocPVA="cd ~/epics-train/examples/iocBoot/iocPVA; ./st.cmd"
 alias start_iocfishtank="cd ~/CSS-Workspaces/Default/Display\ Builder/fishtank; sh st.cmd"
 alias start_imagedemo="~/epics-train/tools/EPICSV4Sandbox/ntndarrayServer/bin/linux-x86_64/ntndarrayServerMain IMAGE"
 alias start_neutrondemo="~/epics-train/tools/EPICSV4Sandbox/neutronsDemoServer/bin/linux-x86_64/neutronServerMain"
-
