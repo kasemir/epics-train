839655066c777c04811a1139fa860ced96d4b001
training <training@training.epics>
2018-08-28 13:49:30 -0400

    Test fishtank

--- tools/profile.d/4_css.sh
+++ tools/profile.d/4_css.sh
@@ -1,3 +1,6 @@
 
 alias phoebus=~/epics-train/tools/start_phoebus.sh
 alias css=~/epics-train/tools/start_phoebus.sh
+
+alias start_iocfishtank="cd /home/training/epics-train/examples/Display\ Builder/fishtank; sh st.cmd"
+
--- tools/start_phoebus.sh
+++ tools/start_phoebus.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-SETTINGS="-settings /home/training/epics-train/tools/phoebus.ini"
 export JAVA_HOME=/home/training/epics-train/tools/jdk-10
 export PATH=$JAVA_HOME/bin:$PATH
 cd ~/epics-train/tools/phoebus-0.0.1
-./phoebus.sh $SETTINGS "$@" >/tmp/phoebus.log 2>&1
+
+
+JAR=product-0.0.1.jar
+
+OPT="-settings /home/training/epics-train/tools/phoebus.ini"
+# To get one instance, use server mode
+#OPT="$OPT -server 4918"
+
+java --add-modules=java.corba -jar $JAR $OPT $SETTINGS "$@" >/tmp/phoebus.log 2>&1 &
