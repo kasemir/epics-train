eb81ca8843348f9aed8d6f1bd95185d62c61c6ac
training <training@training.epics>
2018-08-28 10:08:56 -0400

    Add neutron & image demo

--- tools/profile.d/3_example_iocs.sh
+++ tools/profile.d/3_example_iocs.sh
@@ -3,4 +3,9 @@ alias start_iocExample="cd ~/epics-train/examples/iocBoot/iocExample; ./st.cmd"
 
 alias start_iocCombined="cd ~/epics-train/examples/iocBoot/iocCombined; ./st.cmd"
 
+alias start_imagedemo="~/epics-train/tools/EPICSV4Sandbox/ntndarrayServer/bin/$EPICS_HOST_ARCH/ntndarrayServerMain IMAGE"
+
+alias start_neutrondemo="~/epics-train/tools/EPICSV4Sandbox/neutronsDemoServer/bin/$EPICS_HOST_ARCH/neutronServerMain"
+
+
 
