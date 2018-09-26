f4d540f57b240af451c204ec5820af413ae61261
training <training@training.epics>
2018-09-26 11:16:55 -0400

    Explain settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,11 @@
+# Invoked from ~/.bashrc as
+#   source /home/training/epics-train/settings.sh
 
+# In the past, a file like this contained all the settings.
+#
+# To simplify maintenance, that has been split into
+# several profile.d/*.sh
+# which are all parsed here in alphabetical order
 for i in $HOME/epics-train/tools/profile.d/*.sh
 do
     source $i
