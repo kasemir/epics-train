3d9c3cdfea409439f58b8c8f121ed01f0cff6c58
training <training@training.epics>
2018-09-26 11:23:08 -0400

    toggle_fishtank

--- tools/profile.d/4_css.sh
+++ tools/profile.d/4_css.sh
@@ -3,4 +3,4 @@ alias phoebus=~/epics-train/tools/start_phoebus.sh
 alias css=~/epics-train/tools/start_phoebus.sh
 
 alias start_iocfishtank="cd /home/training/epics-train/examples/Display\ Builder/fishtank; sh st.cmd"
-
+alias toggle_fishtank="while true; do caput training:setpoint 60; sleep 60; caput training:setpoint 25; sleep 60; done"
