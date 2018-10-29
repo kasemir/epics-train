4095bc679a337998871469e467fa690f3de51bbe
training <training@training.epics>
2018-10-29 10:28:26 -0400

    Use neutrons -r

--- tools/profile.d/3_example_iocs.sh
+++ tools/profile.d/3_example_iocs.sh
@@ -5,7 +5,7 @@ alias start_iocCombined="cd ~/epics-train/examples/iocBoot/iocCombined; ./st.cmd
 
 alias start_imagedemo="~/epics-train/tools/EPICSV4Sandbox/ntndarrayServer/bin/$EPICS_HOST_ARCH/ntndarrayServerMain IMAGE"
 
-alias start_neutrondemo="~/epics-train/tools/EPICSV4Sandbox/neutronsDemoServer/bin/$EPICS_HOST_ARCH/neutronServerMain"
+alias start_neutrondemo="~/epics-train/tools/EPICSV4Sandbox/neutronsDemoServer/bin/$EPICS_HOST_ARCH/neutronServerMain -r"
 
 
 
