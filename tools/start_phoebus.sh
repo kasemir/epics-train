e8c6eb8097b7e2ff69f957dcd2bff98586ac000b
training <training@training.epics>
2018-11-15 15:36:30 -0500

    JDK11

--- tools/start_phoebus.sh
+++ tools/start_phoebus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/home/training/epics-train/tools/jdk-10
+export JAVA_HOME=/home/training/epics-train/tools/jdk-11
 export PATH=$JAVA_HOME/bin:$PATH
 cd ~/epics-train/tools/phoebus-0.0.1
 
@@ -11,4 +11,4 @@ OPT="-settings /home/training/epics-train/tools/phoebus.ini"
 # To get one instance, use server mode
 #OPT="$OPT -server 4918"
 
-java -Dlogback.configurationFile=/home/training/epics-train/tools/logback.xml -Dprism.forceGPU=true -Dorg.csstudio.javafx.rtplot.update_counter=false --add-modules=java.corba -jar $JAR $OPT $SETTINGS "$@" &
+java -Dlogback.configurationFile=/home/training/epics-train/tools/logback.xml -Dprism.forceGPU=true -Dorg.csstudio.javafx.rtplot.update_counter=false -Djdk.gtk.version=3 -jar $JAR $OPT $SETTINGS "$@" &
