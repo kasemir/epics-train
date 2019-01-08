4cc6ab8dc4f0d9cac9f71ffecbf60a2ab0b93b80
training <training@training.epics>
2019-01-08 14:45:38 -0500

    Java 11

--- tools/profile.d/4_vdct.sh
+++ tools/profile.d/4_vdct.sh
@@ -1,1 +1,1 @@
-alias vdct="/home/training/epics-train/tools/jdk-10/bin/java -cp ~/epics-train/tools/VisualDCT-2.8.1/VisualDCT.jar com.cosylab.vdct.VisualDCT ~/epics-train/tools/ether_ip/dbd/eipIoc.dbd"
+alias vdct="/home/training/epics-train/tools/jdk-11/bin/java -cp ~/epics-train/tools/VisualDCT-2.8.1/VisualDCT.jar com.cosylab.vdct.VisualDCT ~/epics-train/tools/ether_ip/dbd/eipIoc.dbd"
