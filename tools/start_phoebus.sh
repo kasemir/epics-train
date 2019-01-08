531caed519720a0eb8cccd869c7a74a5cf0acaa6
training <training@training.epics>
2019-01-08 13:16:36 -0500

    GTK2

--- tools/start_phoebus.sh
+++ tools/start_phoebus.sh
@@ -8,8 +8,7 @@ cd ~/epics-train/tools/phoebus-0.0.1
 JAR=product-0.0.1.jar
 
 OPT="-settings /home/training/epics-train/tools/phoebus.ini"
-OPT+=" -Djdk.gtk.version=2"
 # To get one instance, use server mode
 #OPT="$OPT -server 4918"
 
-java -Dlogback.configurationFile=/home/training/epics-train/tools/logback.xml -Dprism.forceGPU=true -Dorg.csstudio.javafx.rtplot.update_counter=false -Djdk.gtk.version=3 -jar $JAR $OPT $SETTINGS "$@" &
+java -Dlogback.configurationFile=/home/training/epics-train/tools/logback.xml -Dprism.forceGPU=true -Dorg.csstudio.javafx.rtplot.update_counter=false -Djdk.gtk.version=2 -jar $JAR $OPT $SETTINGS "$@" &
