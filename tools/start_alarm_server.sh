17afed653b7854eab4d93fd09c1d8f071bca957b
training <training@training.epics>
2019-01-10 14:05:18 -0500

    JDK11

--- tools/start_alarm_server.sh
+++ tools/start_alarm_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/home/training/epics-train/tools/jdk-10
+export JAVA_HOME=/home/training/epics-train/tools/jdk-11
 export PATH="$JAVA_HOME/bin:$PATH"
 
 V="0.0.1"
@@ -8,4 +8,4 @@ V="0.0.1"
 JAR="service-alarm-server-${V}.jar"
 
 cd //home/training/epics-train/tools/alarm-server-0.0.1
-java --add-modules=java.corba -jar $JAR "$@"
+java -jar $JAR "$@"
--- tools/start_archive_engine.sh
+++ tools/start_archive_engine.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export JAVA_HOME=/home/training/epics-train/tools/jdk-10
+export JAVA_HOME=/home/training/epics-train/tools/jdk-11
 export PATH=$JAVA_HOME/bin:$PATH
 
 TOP=/home/training/epics-train/tools
 JAR=archive-engine-0.0.1/service-archive-engine-0.0.1.jar
 
 cd $TOP
-java --add-modules=java.corba -jar $JAR "$@"
+java -jar $JAR "$@"
