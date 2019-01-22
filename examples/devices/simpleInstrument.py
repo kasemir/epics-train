859cf6be1420175c8e634eb2b19dd22cd5a09325
training <training@training.epics>
2019-01-22 10:01:05 -0500

    Stream examples

--- examples/devices/simpleInstrument.py
+++ examples/devices/simpleInstrument.py
@@ -15,9 +15,11 @@ def clip(v):
     return v
 
 status = { }
+verbose = "--verbose" in sys.argv
 class DummyDevice(SocketServer.StreamRequestHandler):
     def handle(self):
         global status
+        global verbose
         client = self.client_address[0]
         if (status.has_key(client)):
             voltage = status[client]['volts']
@@ -34,34 +36,41 @@ class DummyDevice(SocketServer.StreamRequestHandler):
             if(len(line) <= 0):
                break
             reply = None
+            if verbose:
+                print "> " + line
             if (line == '*IDN?'):
-                reply = 'US-PAS Instrument, Yoyodyne Incorporated -- An ACME Industries subsidiary, S/N:1313'
+                reply = '*IDN US-PAS Instrument, Yoyodyne Incorporated -- An ACME Industries subsidiary, S/N:1313'
             elif (line == 'ON?'):
-                reply = '1' if on else '0'
+                reply = 'ON ' + '1' if on else '0'
             elif (line == 'VOLTS?'):
-                reply = '%.4f' % (voltage)
+                reply = 'VOLTS %.4f' % (voltage)
             elif (line == 'CURR?'):
                 ma = voltage + random.gauss(0, 1) if on else 0
-                reply = '%.5g' % (ma)
+                reply = 'CURR %.5g' % (ma)
             elif (line == 'LOAD?'):
                 load = os.getloadavg()
-                reply = '%.5g %.5g %.5g' % (load[0], load[1], load[2])
+                reply = 'LOAD %.5g %.5g %.5g' % (load[0], load[1], load[2])
             elif (len(args) > 1):
                 try:
                     val = float(args[1])
                     if (args[0] == 'ON'):
                         if args[1] == '1':
                             on = True
+                            reply = 'ON 1'
                         elif args[1] == '0':
                             on = False
+                            reply = 'ON 0'
                     elif (args[0] == 'VOLTS'):
                         voltage = clip(val)
+                        reply = 'VOLTS ' + str(float(voltage))
                     status[client]['volts'] = voltage
                     status[client]['on'] = on
                 except:
                     pass
             if (reply):
                 self.wfile.write(reply + '\r\n')
+                if verbose:
+                    print "< " + reply
 
 class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     daemon_threads = True
