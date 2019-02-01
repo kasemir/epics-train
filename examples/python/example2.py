dcf9da4c4bf5eeacfe76fe34cf6413b4284230a2
training <training@training.epics>
2019-02-01 11:33:49 -0500

    clarify

--- examples/python/example2.py
+++ examples/python/example2.py
@@ -5,11 +5,11 @@ c = pvaccess.Channel('training:calc1')
 def echo(value):
     print("Received " + str(value))
 
-c.subscribe('echo', echo)
+c.subscribe('echosub', echo)
 c.startMonitor()
 
 time.sleep(5)
 
 c.stopMonitor()
-c.unsubscribe('echo')
+c.unsubscribe('echosub')
 
